58096f822b9f62b5b23635d49cce0287aeaef840
DevJonn01 <jonatasmarianosouza@gmail.com>
2026-03-24 21:24:02 -0300

    new

--- exercício-004/script.js
+++ exercício-004/script.js
@@ -3,27 +3,24 @@ function contar(){
     let ini = document.querySelector('#inicio')
     let fim = document.querySelector('#fim')
     let passo = document.querySelector('#passo')
-    var res = document.querySelector('.res')
+    let res = document.querySelector('.res')
 
      if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
          alert('[ERROR] Campos Vazios')
     }else{
         res.innerHTML ='contando'
        let i = Number(ini.value)
-       let fi = Number(fim.value)
-       let pass = Number(passo.value)
-       for(let c = i; c <= fi; c += pass){
-           res.innerHTML += c + '✅'
-       }
-     
+       let f = Number(fim.value)
+       let p = Number(passo.value)
+
     }
-    }
+}
     
        
        
     
     
-   }
+   
    
 
  
