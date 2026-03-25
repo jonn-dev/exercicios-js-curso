22334b02cfa5cc9217fcf5908bbf6988aabf46a6
DevJonn01 <jonatasmarianosouza@gmail.com>
2026-03-25 20:03:23 -0300

    ex novo

---
+++ exercicio-005/scripts.js
@@ -0,0 +1,16 @@
+function tabuada(){
+    let num = document.getElementById('num')
+    let tab = document.querySelector('#seltab')
+
+    if(num.value.length == 0){
+        alert('[ERROR] Campo Vazio')
+    }else{
+        let n = Number(num.value)
+
+        let c = 1
+        while(c <= 10){
+            let item = document.createElement('option')
+            
+        }
+    }
+}
--- exercício-004/script.js
+++ exercício-004/script.js
@@ -8,16 +8,36 @@ function contar(){
      if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
          alert('[ERROR] Campos Vazios')
     }else{
-        res.innerHTML ='contando'
+        res.innerHTML ='contando </br>'
        let i = Number(ini.value)
        let f = Number(fim.value)
        let p = Number(passo.value)
+       if(p <= 0){
+        alert('Passo Invalido considerando passo 1')
+        p = 1
+       }
+
+       if(i < f){
+        for(let c = i; c <= f; c+= p){
+            res.innerHTML += c + '👉'
+        }
+        
+       }else{
+        for(let c = i; c >= f; c-= p){
+            res.innerHTML += c + '👉'
+        }
+        
+       }
+       res.innerHTML += '🏁'
+    }
 
     }
-}
+
+       
+
     
        
-       
+
     
     
    
